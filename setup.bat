259aef787f0bb58a5839490304b4d13b886393dd
shindy-dev <shindy.developer@gmail.com>
2021-06-16 22:51:25 +0900

    init

---
+++ setup.bat
@@ -0,0 +1,56 @@
+@echo off
+
+rem ### const value Start ###
+set WARNING=WARNING: 
+set ERROR=ERROR: 
+rem ### const value  End  ###
+
+rem venv 作成パス
+set VENVPATH="%~dp0venv"
+rem venv スクリプトパス
+set SCRIPTSPATH="%VENVPATH%\Scripts"
+
+rem This file is UTF-8 encoded, so we need to update the current code page while executing it
+for /f "tokens=2 delims=:." %%a in ('"%SystemRoot%\System32\chcp.com"') do (
+    set _OLD_CODEPAGE=%%a
+)
+if defined _OLD_CODEPAGE (
+    "%SystemRoot%\System32\chcp.com" 65001 > nul
+)
+
+echo ### Start Setup. ###
+
+echo|set /p="python install ... "
+where /Q python || ( echo. && decho %ERROR%python has not been installed! && pause && exit /b )
+echo ok
+
+rem venv が作成されていれば venv は作成しない
+echo|set /p="create venv ... "
+if not exist %VENVPATH% (
+    rem venv をこのスクリプトと同階層に作成
+    call python -m venv %VENVPATH%
+    echo ok
+) else (
+    echo ok
+    echo %WARNING%Already exist venv! Please remove %VENVPATH% if you want to clean setup.
+)
+
+rem venv をアクティブ化
+call %SCRIPTSPATH%\activate
+
+echo|set /p="install library ... "
+rem ライブラリインストール
+call pip --disable-pip-version-check --quiet install -r %~dp0requirements.txt
+echo ok
+
+echo.
+rem インストール後の環境
+call pip --disable-pip-version-check list
+
+rem venv を非アクティブ化
+call %SCRIPTSPATH%\deactivate
+
+echo.
+echo ### Finished Setup. ###
+
+pause
---
+++ setup.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# ### const value Start ###
+WARNING="WARNING:"
+ERROR="ERROR:"
+# ### const value  End  ###
+
+
+# venv 作成パス
+VENVPATH="`cd $(dirname ${0}) && pwd`/venv"
+# venv スクリプトパス
+SCRIPTSPATH="$VENVPATH/bin"
+
+echo "###  Start Setup.  ###"
+
+# venv が作成されていれば venv は作成しない
+echo -n "create venv ... "
+if [ ! -e $VENVPATH ]; then
+    if [ "$(uname)" == "Darwin" ]; then
+        python3 -m venv $VENVPATH
+    elif [ "$(expr substr $(uname -s) 1 5)" == "MINGW" ]; then
+        python -m venv $VENVPATH
+    elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+        python3 -m venv $VENVPATH
+    else
+        "Unknown OS"
+        exit 0
+    fi
+    echo "ok"
+else
+    echo "ok"
+    echo "$WARNING Already exist venv! Please remove $VENVPATH if you want to clean setup."
+fi
+
+# venv を有効化
+if [ "$(uname)" == "Darwin" ]; then
+    source "$VENVPATH/bin/activate"
+elif [ "$(expr substr $(uname -s) 1 5)" == "MINGW" ]; then
+    . "$VENVPATH/Scripts/activate"
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    source "$VENVPATH/bin/activate"
+fi
+
+echo -n "install library ... "
+# ライブラリインストール
+pip --disable-pip-version-check --quiet install -r "`cd $(dirname ${0}) && pwd`/requirements.txt"
+echo "ok"
+
+echo -e
+# インストール後の環境
+pip --disable-pip-version-check list
+
+# venv を非アクティブ化
+if [ "$(uname)" == "Darwin" ]; then
+    source "$VENVPATH/bin/deactivate"
+elif [ "$(expr substr $(uname -s) 1 5)" == "MINGW" ]; then
+    deactivate
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    source "$VENVPATH/bin/deactivate"
+fi
+
+echo -e 
+echo "###  Finished Setup.  ###"
+exit 0
